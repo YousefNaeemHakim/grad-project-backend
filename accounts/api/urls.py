a571d20a3347129ea78dcbc2ad1fc420caecce99
YousefNaeemHakim <yousefnaeem22@gmail.com>
2023-01-27 07:49:17 +0200

    Minor Changes

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,6 +1,7 @@
-from knox import views as knox_views
-from ..views import LoginAPI, RegisterAPI, UserAPI, ChangePasswordView
 from django.urls import path
+from knox import views as knox_views
+from .views import LoginAPI, RegisterAPI, UserAPI
+# from .views import ChangePasswordView
 
 urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
@@ -8,5 +9,5 @@ urlpatterns = [
     path('logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
     path('user/', UserAPI.as_view(), name='user'),
-    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    # path('change-password/', ChangePasswordView.as_view(), name='change-password'),
 ]
