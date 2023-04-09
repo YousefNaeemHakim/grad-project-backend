5edba41d029c9f2dbacbfb375c2cf1b6d2e673fb
YousefNaeemHakim <yousefnaeem22@gmail.com>
2023-04-09 21:31:10 +0200

    Cleaning the old project

--- base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Topic , Group , Post
-
-admin.site.register(Topic)
-admin.site.register(Group)
-admin.site.register(Post)
-# admin.site.register(Profile)
