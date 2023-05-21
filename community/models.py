aaf051ccaefc00325b31313cb51de6e7b3f7dbd0
Omar Khaled <82944109+omarkhaled747474@users.noreply.github.com>
2023-05-21 13:23:03 +0300

    Update models.py

--- community/models.py
+++ community/models.py
@@ -6,6 +6,7 @@ import uuid
 class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=30)
     bio = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
