d022d6a2aff05460b3ec402c1162643946bb8c0b
Omar Khaled <82944109+omarkhaled747474@users.noreply.github.com>
2023-05-21 15:26:13 +0300

    Add files via upload

---
+++ base/api/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+# Summary : 
+# get post
+path('summary_mixins/', views.mixins_list.as_view()),  # ليه ال as view مش ظاهره
+# get update delete
+path('summary_mixins/<int:pk>', views.mixins_pk.as_view()),
+
+# Review : 
+# get post
+path('review_mixins/', views.mixins_list.as_view()),
+# get update delete
+path('review_mixins/<int:pk>', views.mixins_pk.as_view()),
+
+# Category : 
+# get post
+path('category_mixins/', views.mixins_list.as_view()),
+# get update delete
+path('category_mixins/<int:pk>', views.mixins_pk.as_view()),
+    
+# Subscription : 
+# get post
+path('subscription_mixins/', views.mixins_list.as_view()),
+# get update delete
+path('subscription_mixins/<int:pk>', views.mixins_pk.as_view()),
+    
+]
+
---
+++ base/api/views.py
@@ -0,0 +1,34 @@
+from rest_framework import generics, mixins
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAdminUser
+
+# mixins
+class mixins_list(mixins.ListModelMixin, mixins.CreatModelMixin, generics.GenericAPIView) : 
+    queryset = User.objects.all() # هل احنا بنختار اليوزر كل مره ولا واشمعنا
+    serializer_class = UserSerializer
+
+# get/post
+    def get(self, request) : 
+        return self.list(request)
+
+    def post(self, request) : 
+        return self.create(request)
+
+# get/update/delete
+class mixins_pk(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView) :
+      queryset = User.objects.all()
+      serializer_class = UserSerializer
+
+      def get(self, request, pk) : 
+        return self.retrieve(request)
+
+      def put(self, request, pk) : 
+        return self.update(request)
+
+      def delete(self, request, pk) : 
+        return self.destroy(request)
+  
+
+
+
